1a23b9d5ab86a20b2fbcacb5c076b0e07ceeaf82
starmanone <69840505+starmanone@users.noreply.github.com>
2022-01-03 18:54:11 +0100

    Update

---
+++ exercise_1.cpp
@@ -0,0 +1,36 @@
+// What this algorithm does?
+// Calculate complexity
+
+#include <stdio.h>
+
+int mS (int A[], int n);
+
+int main (){
+
+    int Vector[] = {22, 43, 4, 3, 2, 87, 55, 7, 35, 54};
+
+    printf("Result: %d \n", mS(Vector, 10));
+
+    return 0;
+
+}
+
+int mS (int A[], int n){
+
+    int max = 0;
+
+    for (int left = 1; left < n; left++)
+    {
+        int sum = 0;
+        for (int right = left; right < n; right++)
+        {
+           sum = sum + A[right];
+           if (sum > max) {
+               max = sum;
+           }
+        }
+    }
+    
+    return max;
+
+}
